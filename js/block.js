52a9941028c10a0651a856416816644c01f20162
mpaulweeks <mpaulweeks@gmail.com>
2018-12-31 12:47:01 -0500

    borders on cubes

--- js/block.js
+++ js/block.js
@@ -5,22 +5,36 @@ function range(delta){
 class Block {
   constructor(scene, position, tickOffset, settings) {
     this.scene = scene;
-    this.geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    this.model = new THREE.Mesh(this.geometry, this.material);
-    this.model.translateX(position.x);
-    this.model.translateY(position.y);
-    this.model.translateZ(position.z);
+    const cubeSide = 0.4;
+    const geometry = new THREE.BoxBufferGeometry(cubeSide, cubeSide, cubeSide);
+    this.material = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      polygonOffset: true,
+      polygonOffsetFactor: 1, // positive value pushes polygon further away
+      polygonOffsetUnits: 1,
+    });
+    this.mesh = new THREE.Mesh(geometry, this.material);
+    this.mesh.translateX(position.x);
+    this.mesh.translateY(position.y);
+    this.mesh.translateZ(position.z);
+
+    // https://stackoverflow.com/a/31541369
+    const geo = new THREE.EdgesGeometry( geometry ); // or WireframeGeometry
+    const mat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 2 } );
+    const wireframe = new THREE.LineSegments( geo, mat );
+    this.mesh.add( wireframe );
+
     this.tickOffset = tickOffset;
     this.settings = settings;
 
     // setup
-    this.model.block = this;
-    this.scene.add(this.model);
+    this.mesh.block = this;
+    this.scene.add(this.mesh);
   }
   rotate() {
-    this.model.rotation.x += 0.01;
-    this.model.rotation.y += 0.01;
+    // this.mesh.rotation.x += 0.01;
+    // this.mesh.rotation.y += 0.01;
+    this.mesh.rotation.z += 0.01;
   }
   paint(tick) {
     this.material.color = Rainbow.getColor(this.tickOffset + tick);
@@ -32,13 +46,13 @@ class Block {
     return new Block(scene, {x: 0, y: 0, z: 0}, 0, {});
   }
   clone() {
-    const positionSpread = 3;
+    const positionSpread = 1;
     return new Block (
       this.scene,
       {
-        x: this.model.position.x + range(positionSpread),
-        y: this.model.position.y + range(positionSpread),
-        z: this.model.position.z + range(positionSpread),
+        x: this.mesh.position.x + range(positionSpread),
+        y: this.mesh.position.y + range(positionSpread),
+        z: this.mesh.position.z + range(positionSpread / 10),
       },
       this.tickOffset + range(10),
       this.settings
